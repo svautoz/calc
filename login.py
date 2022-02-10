2c4cc3bc8343767f33e2e01e4ba33f266dd26355
User <C:\Users\User\AppData\Roaming\The Bat!>
2022-02-10 21:44:59 +0300

    log mod

--- login.py
+++ login.py
@@ -1,3 +1,3 @@
 def log_in_file(number_1, number_2, oper, result):
     with open('./logging.csv','a') as file:
-        file.write(f'{number_1};{oper};{number_2};{result}')
+        file.write(f'{number_1};{oper};{number_2};{result};\n')
